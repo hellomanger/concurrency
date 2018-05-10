58c7affbda96d47daa751ded7f546c534e0ce678
N-264 <N-264@chengzhiyong>
2018-05-10 18:27:43 +0800

    发反反复复

--- concurrency/src/main/java/com/carlton/concurrency/distributed/DistributedClient.java
+++ concurrency/src/main/java/com/carlton/concurrency/distributed/DistributedClient.java
@@ -4,30 +4,27 @@ import org.redisson.Redisson;
 import org.redisson.api.RRemoteService;
 import org.redisson.api.RedissonClient;
 import org.redisson.api.RemoteInvocationOptions;
-import org.redisson.remote.RemoteServiceAck;
 
 import com.carlton.concurrency.service.HelloService;
 
 public class DistributedClient {
 	public static void main(String[] args) {
 		RedissonClient redisson = Redisson.create();
+		RRemoteService remoteService = redisson.getRemoteService();
+		HelloService service = remoteService.get(HelloService.class);
 		for(int i=0;i<10;i++) {
 			try {
-				RRemoteService remoteService = redisson.getRemoteService();
-				HelloService service = remoteService.get(HelloService.class);
 				String result = service.sayHello("tom"+i);
 				System.out.println(result+"     i "+i);
 			}catch (Exception e) {
 				RemoteInvocationOptions options = RemoteInvocationOptions.defaults().noAck();
-				RRemoteService remoteService = redisson.getRemoteService();
-				HelloService service = remoteService.get(HelloService.class,options);
+				remoteService = redisson.getRemoteService();
+				service = remoteService.get(HelloService.class,options);
 				System.out.println(e.getMessage());
 				String result = service.sayHello("tom"+(i+1));
 				System.out.println(result+"     i "+(i+1));
 			}
-			
-			
 		}
-		
+//		AbstractQueuedSynchronizer
 	}
 }
